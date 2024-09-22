4f4a7c5646bf5dd4e660558f1c0847f43ea9d4e8
Milap Narola <milap5322@gmail.com>
2024-09-22 16:02:09 +0530

    Add

---
+++ MVC-01/middlewares/auth.js
@@ -0,0 +1,12 @@
+
+const isAuth = (req, res, next) => {
+    let { id } = req.cookies
+    if (id) {
+        return next()
+    }
+    else {
+        return res.redirect("/user/login")
+    }
+}
+
+module.exports =isAuth
